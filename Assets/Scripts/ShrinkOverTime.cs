8f77049cbf3619eb00c032e3fda368534eb947da
Magnus Tullberg <flummeltrollet@gmail.com>
2018-08-11 13:09:02 +0200

    add core shrink system

---
+++ Assets/Scripts/ShrinkOverTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkOverTime : MonoBehaviour {
+
+    bool isShrinking = false;
+    Transform trans;
+    float speed = 0.01f;
+    float minSize = 0.1f;
+    float intervall = 0.2f;
+    float startTime = 5f;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        trans = this.GetComponent<Transform>();
+        yield return new WaitForSeconds(startTime);
+        StartCoroutine(Shrink(intervall, minSize));
+	}
+
+    /* Shrinks every "intervall" untill "minSize" is reached */
+    IEnumerator Shrink(float intervall, float minSize) {
+        Debug.Log(trans.localScale.x);
+        while (trans.localScale.x > minSize) {
+            yield return new WaitForSeconds(intervall);
+            trans.localScale -= new Vector3(speed, 0, 0);
+        }
+    }
+}
